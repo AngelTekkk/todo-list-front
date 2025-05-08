99ad081a4b4740a9a00fd8ddc480b9d959b75afa
Eugen Gerk <eugen.gerk@tn.mischok.academy.de>
2025-05-08 10:55:07 +0200

    dashboard scss und navbar links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,10 +14,10 @@ function Header() {
             <h1 className={s.h1}><a className="h1-link" href="https://www.youtube.com/watch?v=pOWCtIv9LK8" target="_blank" rel="noopener noreferrer">🥚🍷🍰 ToDo's</a></h1>
             <nav>
                 <ul className={s.navList}>
-                    <li className={s.navPoint}><a className={s.navLink} href='#'>MY TODO'S</a></li>
-                    <li className={s.navPoint}><a className={s.navLink} href='#'>PROJECTS</a></li>
-                    <li className={s.navPoint}><a className={s.navLink} href='#'>CURRICULUM</a></li>
-                    <li className={s.navPoint}><a className={s.navLink} href='#'>COMPLETED</a></li>
+                    <li className={s.navPoint}><a className={s.navLink} href='/todos'>MY TODO'S</a></li>
+                    <li className={s.navPoint}><a className={s.navLink} href='/projects'>PROJECTS</a></li>
+                    <li className={s.navPoint}><a className={s.navLink} href='/curriculum'>CURRICULUM</a></li>
+                    <li className={s.navPoint}><a className={s.navLink} href='/completed'>COMPLETED</a></li>
                 </ul>
             </nav>
                 <button className={s.logoutBtn} onClick={handleLogout}>LOGOUT</button>
